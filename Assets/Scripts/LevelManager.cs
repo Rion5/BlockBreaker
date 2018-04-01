9bf8d0fa5daf1049d5bfda6bc4d91b246d6f6679
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-04-01 11:30:41 -0400

    Initial Upload

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour {
+    //Intent: When the Start Button is pressed, Start the Game
+    public void LoadLevel(string name)
+    {
+        Debug.Log("Level Load Requested: " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game Button Pressed");
+    }
+}
