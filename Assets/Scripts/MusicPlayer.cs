2815a3b7e78a8cb67d769600c24ecc526fa38e7b
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-04-02 20:40:41 -0400

    Add GameAudio & LoseCollider

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+    private static MusicPlayer instance = null;
+    private void Awake()
+    {
+        print("MusicPlayer: Awake()");
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            print("Duplicate Music Player Destroyed");
+        }
+        else
+        {
+            instance = this;
+            GameObject.DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        print("MusicPlayer: Start()");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
