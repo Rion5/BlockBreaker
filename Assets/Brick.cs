ee9962a1c7dd8ddff9eb2fc47685f54e2e00f0c4
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-04-04 17:51:27 -0400

    Add: First Level

---
+++ Assets/Brick.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Brick : MonoBehaviour {
+
+    //How many times the brick can be hit (I.E its health)
+    public int maxHits;
+
+    private int timesHit;
+
+	// Use this for initialization
+	void Start () {
+        timesHit = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        timesHit++;
+    }
+}
