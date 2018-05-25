b40ba4b259108dbdd3c8408f8ffdbd6f86f43998
Rion5 <31965265+Rion5@users.noreply.github.com>
2018-05-24 22:27:22 -0400

    Add: Random tweak for ball bounce

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -37,8 +37,12 @@ public class Ball : MonoBehaviour {
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
+        Vector2 randomTweak = new Vector2(Random.Range(0f, 0.2f), Random.Range(0f, 0.2f));
+
         if (hasStarted) { 
             GetComponent<AudioSource>().Play();
+            rigidbody2D.velocity += randomTweak;
+            print(randomTweak);
         }
     }
 }
